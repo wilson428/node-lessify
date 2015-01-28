66a83588f8bfa61e13bd33610ed0b0358386acfc
Chris Wilson <christopher.e.wilson@gmail.com>
2015-01-28 11:25:13 -0500

    Ingest options from package.json using the new method of [ "node-lessify", { "textMode": true } ] in package.json.

--- index.js
+++ index.js
@@ -8,12 +8,6 @@ var textMode = false,
 	func_start = "(function() { var head = document.getElementsByTagName('head')[0]; var style = document.createElement('style'); style.type = 'text/css';",
 	func_end = "if (style.styleSheet){ style.styleSheet.cssText = css; } else { style.appendChild(document.createTextNode(css)); } head.appendChild(style);}())"; 
 
-try {
-	var options = require(process.cwd() + "/package.json");
-} catch (e) {
-	var options = {};
-};
-
 /*
 you can pass options to the transform from your package.json file like so: 
     "browserify": {
@@ -21,7 +15,20 @@ you can pass options to the transform from your package.json file like so:
         	"node-lessify": "textMode"
         }
     }
+	NOTE: This is deprecated since it is now possible to do this like so:
+
+    "browserify": {
+		"transform": [
+			[ "node-lessify", { "textMode": true } ]
+		]
+	}
 */
+
+try {
+	var options = require(process.cwd() + "/package.json");
+} catch (e) {
+	var options = {};
+};
 
 /*
 textMode simply compiles the LESS into a single string of CSS and passes it back without adding the code that automatically appends that CSS to the page
@@ -31,10 +38,19 @@ if (options.browserify && options.browserify["transform-options"] && options.bro
 	textMode = true;
 }
 
-module.exports = function(file) {
+module.exports = function(file, package_options) {
 	if (!/\.css$|\.less$/.test(file)) {
 		return through();
 	}
+
+	// supplement options with those from package
+	for (var key in package_options) {
+		if (package_options.hasOwnProperty(key) && key !== "_flags") {
+			options[key] = package_options[key];
+		}
+	}
+
+	console.error(package_options);
 
 	var buffer = "", mydirName = path.dirname(file);
 
